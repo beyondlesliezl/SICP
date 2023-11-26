80eddbceb7804cc1f23028ed5f84410bab3a4bbc
puyu <beyondlesliezl@icloud.com>
2023-11-26 11:08:47 +0800

    finish 1.12

--- Chapter1/1.12.rkt
+++ Chapter1/1.12.rkt
@@ -1,1 +1,15 @@
 #lang sicp
+
+(define (pascal-row n)
+  (define (next-row lst)
+    (if (= (length lst) 1)
+        (list (car lst) (car lst))
+        (let ((ret (next-row (cdr lst)))
+              (first (car lst)))
+              (cons first (cons (+ first (car ret) (cdr ret)))))))
+  (if (<= n 1)
+      (list 1)
+      (next-row (pascal-row (- n 1)))))
+
+; 在设置base case 之后，就可以引用自身来进行计算了
+; 在此例中，就是 (next-row (cdr lst))
