823cae1dc597ab89d0cdabe96074fa37245abc50
puyu <beyondlesliezl@icloud.com>
2023-12-04 19:33:31 +0800

    2.47

---
+++ Chapter2/2.47.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v) (edge1-frame frame))
+               (scale-vect (ycor-vect v) (edge2-frame frame))))))
+
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+
+(define (origin-frame frame) (car frame))
+(define (edge1-frame frame) (cadr frame))
+(define (edge2-frame frame) (cadr (cdr frame)))
+
+
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
